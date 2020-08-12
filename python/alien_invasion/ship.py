7e55aad016e2c5ed1427168dde2361de480291ca
Matthew Xu <matthew.xu@carel.locale>
2020-08-12 16:35:10 +0800

    0812

--- python/alien_invasion/ship.py
+++ python/alien_invasion/ship.py
@@ -8,3 +8,9 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         # 将每艘新飞船放置在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+    
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
